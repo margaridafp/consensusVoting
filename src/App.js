fc006b0d679ebc6c882cf30b3f2a93aebf2c0ca6
Ana Pereira <anaferraspereira@gmail.com>
2021-01-09 23:13:09 +0000

    Config eslint Closes #2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, { Component, useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Paper, Typography } from "@material-ui/core";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import votingOptions from './components/votingOptions'
+import {Grid, Typography} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import {makeStyles} from '@material-ui/core/styles';
+import React, {useState} from 'react';
+
 import Text from './components/Text';
+import votingOptions from './components/votingOptions';
 
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   container: {
-    display:"flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "stretch",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'stretch',
     padding: theme.spacing(2),
   },
   card: {
@@ -25,50 +23,65 @@ const useStyles = makeStyles(theme => ({
     width: '99%',
   },
   titleText: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontFamily: 'Arial',
   },
   explainedText: {
     fontFamily: 'Arial',
-    color: "textSecondary",
-    textAlign: "justify",
+    color: 'textSecondary',
+    textAlign: 'justify',
   },
-  result:{
+  result: {
     height: '30vw',
     align: 'center',
-    width: '100%'
+    width: '100%',
   },
-  resultText:{
-    fontWeight: "bold",
+  resultText: {
+    fontWeight: 'bold',
     fontFamily: 'Arial',
-    fontSize:'25vw ',
-  }
+    fontSize: '25vw ',
+  },
 }));
-
 
 function Lista(props) {
   const classes = useStyles();
   return (
     <Grid container className={classes.container}>
-      {votingOptions.map(votingOption => (
+      {votingOptions.map((votingOption) => (
         <Grid item key={votingOption.name} sm={6} >
-          <Card variant="outlined" className={classes.card} style={{ background: votingOption.color }}>
+          <Card
+            variant="outlined"
+            className={classes.card}
+            style={{background: votingOption.color}}>
             <CardActionArea onClick={() => props.onDecision(votingOption)}>
               <CardContent>
-                <Typography gutterBottom align="center" component="h5" variant="h5" className={classes.titleText}  >
+                <Typography gutterBottom
+                  align="center"
+                  component="h5"
+                  variant="h5"
+                  className={classes.titleText} >
                   {/* {votingOption.name} */}
 
-                  <Text content={votingOption.name} symbol={votingOption.symbol}></Text>
+                  <Text
+                    content={votingOption.name}
+                    symbol={votingOption.symbol}>
+
+                  </Text>
 
                 </Typography>
-                <Typography color="textSecondary" component="p" className={classes.explainedText}>{votingOption.explained}</Typography>
+                <Typography
+                  color="textSecondary"
+                  component="p"
+                  className={classes.explainedText}>
+                  {votingOption.explained}
+                </Typography>
               </CardContent>
             </CardActionArea>
           </Card>
         </Grid>
       ))}
     </Grid>
-  )
+  );
 }
 
 function App() {
@@ -79,22 +92,34 @@ function App() {
 
   if (name===null) {
     return (
-        <Lista onDecision={(decision) => [setSymbol(decision.symbol), setName(decision.name), setColor(decision.color)]} />
+      <Lista
+        onDecision={(decision) => [
+          setSymbol(decision.symbol),
+          setName(decision.name),
+          setColor(decision.color)]
+        } />
     );
-  } else { 
+  } else {
     return (
       <>
-      <p align="right">
-        <Button variant="contained" onClick={()=>setName(null)}>Back</Button>
-      </p>
-      <Card variant="outlined" className={classes.result} style={{ background: color }}>
-        <Typography gutterBottom component="h1" variant="h1" align='center' className={classes.resultText}>
-          <Text content={symbol} />
-        </Typography>
-      </Card>
+        <p align="right">
+          <Button variant="contained" onClick={()=>setName(null)}>Back</Button>
+        </p>
+        <Card
+          variant="outlined"
+          className={classes.result}
+          style={{background: color}}>
+          <Typography gutterBottom
+            component="h1"
+            variant="h1"
+            align='center'
+            className={classes.resultText}>
+            <Text content={symbol} />
+          </Typography>
+        </Card>
       </>
-      )
-    }
+    );
+  }
 }
 
 export default App;
